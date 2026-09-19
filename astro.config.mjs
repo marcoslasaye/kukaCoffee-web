003297c45509122c4c600d51557ea16751e45955
MarcosLS <marcoslopezsapitri.123@gmail.com>
2026-09-19 10:51:36 +0800

    fix(deps): remove incompatible @astrojs/tailwind peer dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
-﻿import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://kukacoffee.id',
   trailingSlash: 'never',
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
     sitemap(),
   ],
 });
